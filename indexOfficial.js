d03027afe7bd149a9d55ad05732ed6aa6f7e0bf1
Yasirah <91209060+yasirah-tech@users.noreply.github.com>
2021-09-28 10:49:39 -0400

    Create indexOfficial.js
    This is the copy with instructions from Shadeira.

---
+++ indexOfficial.js
@@ -0,0 +1,28 @@
+//1
+function countToFifty(start,end){ 
+ for(var i = 1; i <= 50; i++) {
+    console.log(i);
+}
+
+
+//2
+function countFromOne(num){
+  const array = [];
+  for(let i = 1; i <= num; i++) {
+    array.push(i);
+  }
+ return array 
+ }
+
+//3
+function threeAndFiveInFifty(){
+ for(let i = 0; i <= 50; i++) { 
+  if(i % 3 == 0 && i % 5 == 0)
+  console.log(output)
+  
+}
+
+//4
+function threeAndFive(){
+  
+}
