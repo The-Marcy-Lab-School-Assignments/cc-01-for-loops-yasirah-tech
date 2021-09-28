22f6b7e1f1e9c806bc4b9069261b00a5a12c978c
Yasirah <yasirahboyce1@gmail.com>
2021-09-28 14:44:54 +0000

    code challenge 00

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 //1
-function countToFifty(){
-  
+function countToFifty(start,end){ 
+ for(var i = 1; i <= 50; i++) {
+    console.log(i);
 }
 
+
 //2
-function countFromOne(){
-  
-}
+function countFromOne(num){
+  const array = [];
+  for(let i = 1; i <= num; i++) {
+    array.push(i);
+  }
+ return array 
+ }
 
 //3
 function threeAndFiveInFifty(){
+ for(let i = 0; i <= 50; i++) { 
+  if(i % 3 == 0 && i % 5 == 0)
+  console.log(output)
   
 }
 
@@ -17,5 +26,4 @@ function threeAndFiveInFifty(){
 function threeAndFive(){
   
 }
-
 
